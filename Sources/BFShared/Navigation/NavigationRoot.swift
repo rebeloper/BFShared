78143c4fba626864aaf5e7d138122f7abc30cc3c
rebeloper <therebeloper@gmail.com>
2022-07-06 00:11:19 +0300

    navigation

--- Sources/BFShared/Navigation/NavigationRoot.swift
+++ Sources/BFShared/Navigation/NavigationRoot.swift
@@ -14,11 +14,11 @@ public struct NavigationRoot<R, D, C>: View where R: View, D : Hashable, C : Vie
     let data: D.Type
     @ViewBuilder let destination: (D) -> C
     
-    public init(navigation: Navigation? = nil,
+    public init(navigation: Navigation,
                 _ root: @escaping () -> R,
                 with data: D.Type,
                 @ViewBuilder destination: @escaping (D) -> C) {
-        self.navigation = navigation ?? Navigation()
+        self.navigation = navigation
         self.root = root
         self.data = data
         self.destination = destination
