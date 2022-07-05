3c319e79162b87b3c154fee45d4cd8dcf9a8c789
rebeloper <therebeloper@gmail.com>
2022-07-05 13:17:17 +0300

    ProfileService

---
+++ Sources/BFShared/Service/ProfileService.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileService.swift
+//  
+//
+//  Created by Alex Nagy on 05.07.2022.
+//
+
+import SwiftUI
+import FirebaseService
+import FirebaseAuth
+
+public class ProfileService: ObservableObject {
+    
+    @Published public var admin = Admin()
+    
+    @MainActor
+    public func createAdminIfNonExistent() async throws {
+        admin = try await FirestoreManager.createIfNonExistent(admin, withUid: admin.uid, atPath: Path.Firestore.admins)
+    }
+    
+    public func signIn(withEmail email: String, password: String) async throws {
+        try await Auth.auth().signIn(withEmail: email, password: password)
+    }
+    
+    public func signUp(withEmail email: String, password: String) async throws -> String {
+        try await Auth.auth().createUser(withEmail: email, password: password).user.uid
+    }
+    
+    public func logout() throws {
+        try Auth.auth().signOut()
+    }
+    
+    @MainActor
+    public func fetchAdmin(uid: String) async throws {
+        admin = try await FirestoreManager.read(atPath: Path.Firestore.admins, uid: uid)
+    }
+}
+
