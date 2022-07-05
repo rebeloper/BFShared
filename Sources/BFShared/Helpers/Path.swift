19567182e5ec2339f5cdd5b869c6d7300811d0b6
rebeloper <therebeloper@gmail.com>
2022-07-05 11:02:13 +0300

    Path

---
+++ Sources/BFShared/Helpers/Path.swift
@@ -0,0 +1,16 @@
+//
+//  Path.swift
+//  
+//
+//  Created by Alex Nagy on 05.07.2022.
+//
+
+import Foundation
+
+struct Path {
+    
+    struct Firestore {
+        static let admins = "admins"
+    }
+    
+}
