295bc86152e9b60c1ae30bbbf71cc337a8ac8569
rebeloper <therebeloper@gmail.com>
2022-08-09 11:10:25 +0300

    fetchProducts

--- Sources/BFShared/Service/ProfileService.swift
+++ Sources/BFShared/Service/ProfileService.swift
@@ -94,7 +94,7 @@ public class ProfileService: ObservableObject {
     @MainActor
     public func fetchProducts(for merchant: Merchant) async throws -> [Product] {
         let queryItem = QueryItem("merchantUid", .isEqualTo, merchant.uid)
-        return try await FirestoreManager.query(path: Path.Firestore.products)
+        return try await FirestoreManager.query(path: Path.Firestore.products, queryItems: [queryItem])
     }
 }
 
