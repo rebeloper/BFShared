19b544e0cc503bc3f69d74884cd686de7dfd58be
rebeloper <therebeloper@gmail.com>
2022-08-09 11:04:10 +0300

    Product

---
+++ Sources/BFShared/Models/Product/Product.swift
@@ -0,0 +1,59 @@
+//
+//  Product.swift
+//  
+//
+//  Created by Alex Nagy on 09.08.2022.
+//
+
+import SwiftUI
+import FirebaseService
+import BetterCodable
+import Firebase
+import FirebaseFirestoreSwift
+
+// a product that is created by the Admin for a particular merchant
+struct Product: Codable, Identifiable, Firestorable, Hashable {
+    
+    @DocumentID var id: String?
+    @DefaultEmptyString var uid: String
+    @DefaultTimestamp var createdAt: Timestamp
+    @DefaultEmptyString var name: String
+    @DefaultEmptyString var description: String
+    @DefaultEmptyString var merchantUid: String
+    @DefaultEmptyString var productImageUrl: String
+    @DefaultEmptyString var instructions: String
+    @DefaultFalse var isAvailable: Bool
+    @DefaultZeroInt var price: Int
+    @DefaultZeroInt var maxFlavorsCount: Int
+    
+    init(uid: String? = nil,
+         createdAt: Timestamp? = nil,
+         name: String? = nil,
+         description: String? = nil,
+         merchantUid: String? = nil,
+         productImageUrl: String? = nil,
+         instructions: String? = nil,
+         isAvailable: Bool? = nil,
+         price: Int? = nil,
+         maxFlavorsCount: Int? = nil) {
+        self.uid = uid ?? ""
+        self.createdAt = createdAt ?? Timestamp()
+        self.name = name ?? ""
+        self.description = description ?? ""
+        self.merchantUid = merchantUid ?? ""
+        self.productImageUrl = productImageUrl ?? ""
+        self.instructions = instructions ?? ""
+        self.isAvailable = isAvailable ?? false
+        self.price = price ?? 0
+        self.maxFlavorsCount = maxFlavorsCount ?? 0
+    }
+    
+}
+
+struct DefaultProductStrategy: DefaultCodableStrategy {
+    static var defaultValue: Product { return Product() }
+}
+
+typealias DefaultEmptyProduct = DefaultCodable<DefaultProductStrategy>
+
+
