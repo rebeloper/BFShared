59ce459e8f1510fb1f512e6b9ab0c67d5c6c06f4
rebeloper <therebeloper@gmail.com>
2022-08-11 18:14:21 +0300

    CLLocationCoordinate2D+

---
+++ Sources/BFShared/Helpers/CLLocationCoordinate2D+.swift
@@ -0,0 +1,14 @@
+//
+//  CLLocationCoordinate2D+.swift
+//  
+//
+//  Created by Alex Nagy on 11.08.2022.
+//
+
+import CoreLocation
+
+extension CLLocationCoordinate2D: Identifiable {
+    public var id: String {
+        "\(latitude)-\(longitude)"
+    }
+}
