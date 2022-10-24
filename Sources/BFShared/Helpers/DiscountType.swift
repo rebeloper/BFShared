a3c027def497c330db19492cf63adf14e506de2e
rebeloper <therebeloper@gmail.com>
2022-10-24 12:36:35 +0300

    DiscountType

---
+++ Sources/BFShared/Helpers/DiscountType.swift
@@ -0,0 +1,12 @@
+//
+//  DiscountType.swift
+//  
+//
+//  Created by Alex Nagy on 24.10.2022.
+//
+
+import Foundation
+
+public enum DiscountType: Int {
+    case defaultValue = 0, percentage, amount
+}
