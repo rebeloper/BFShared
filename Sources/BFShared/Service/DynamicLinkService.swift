701462635d0c8cfd2d4428df6abb655b356c67f0
Alex Nagy <rebeloperalpar@gmail.com>
2023-08-28 15:08:35 +0300

    https://dessiapp.page.link

--- Sources/BFShared/Service/DynamicLinkService.swift
+++ Sources/BFShared/Service/DynamicLinkService.swift
@@ -19,9 +19,9 @@ public class DynamicLinkService: ObservableObject {
         static let host = "brainfreeze.app"
         static let path = "/links"
 #if DEBUG
-        static let domainURIPrefix = "https://brainfreezebackenddev.page.link"
+        static let domainURIPrefix = "https://dessiapp.page.link"
 #else
-        static let domainURIPrefix = "https://brainfreezebackend.page.link"
+        static let domainURIPrefix = "https://dessiapp.page.link"
 #endif
         static let imageURL = "https://unsplash.com/photos/Wpg3Qm0zaGk/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8M3x8aWNlY3JlYW18fDB8fHx8MTY0Mzk3MTI3NA&force=true&w=640"
     }
